8f19e55b1d5111a6f46b15b005ae21ad11bea922
Swordf1sh <uripatton@gmail.com>
2016-02-22 13:35:28 -0800

    add splashscreen

--- Moderat.py
+++ Moderat.py
@@ -501,9 +501,51 @@ class MainDialog(QMainWindow, gui.Ui_MainWindow):
         sys.exit(1)
 
 
+class MovieSplashScreen(QSplashScreen):
+
+    def __init__(self, movie, parent = None):
+
+        movie.jumpToFrame(0)
+        pixmap = QPixmap(movie.frameRect().size())
+
+        QSplashScreen.__init__(self, pixmap)
+        self.movie = movie
+        self.movie.frameChanged.connect(self.repaint)
+
+    def showEvent(self, event):
+        self.movie.start()
+
+    def hideEvent(self, event):
+        self.movie.stop()
+
+    def paintEvent(self, event):
+
+        painter = QPainter(self)
+        pixmap = self.movie.currentPixmap()
+        self.setMask(pixmap.mask())
+        painter.drawPixmap(0, 0, pixmap)
+
+    def sizeHint(self):
+
+        return self.movie.scaledSize()
+
+
 # Run Application
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+
+    movie = QMovie("assets\\splash.gif")
+    splash = MovieSplashScreen(movie)
+    splash.show()
+
+    start = time.time()
+
+    while movie.state() == QMovie.Running and time.time() < start + 5:
+        app.processEvents()
+
     form = MainDialog()
     form.show()
+    splash.finish(form)
+
+
     sys.exit(app.exec_())

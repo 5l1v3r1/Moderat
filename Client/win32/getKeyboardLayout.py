87a6fc05d81245cbd600fbb7939fc3eea52d373e
uripa <uripatton@gmail.com>
2016-07-28 22:02:05 +0400

    locale

---
+++ Client/win32/getKeyboardLayout.py
@@ -0,0 +1,13 @@
+import ctypes
+from decimal import *
+
+# 67699721 ENG
+# -257227721 GEO
+# -257752039 RUS
+
+user32 = ctypes.windll.user32
+
+foregroundWindow = user32.GetForegroundWindow()
+tid = user32.GetWindowThreadProcessId(foregroundWindow, 0)
+print user32.GetKeyboardLayout(tid)
+print hex(257752039)

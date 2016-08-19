6e7c96b42267c1bc708cc460d7035ee06f851377
uripa <uripatton@gmail.com>
2016-08-19 18:36:35 +0400

    add Plugins

---
+++ plugins/openUrlInBrowser.py
@@ -0,0 +1,15 @@
+plugin_name = 'openUrlInBrowser'
+
+plugin_description = 'Open File or Url In Browser'
+
+plugin_source = '''
+
+###SETTINGS###
+url = 'URL_HERE'
+###SETTINGS###
+
+
+os.startfile(url)
+mprint = Done
+
+'''

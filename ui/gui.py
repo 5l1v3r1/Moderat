8b4d7406d9e6ee475ae89b131d6e410ecaee2087
uripa <uripatton@gmail.com>
2016-09-18 23:10:27 +0400

    ui update

--- ui/gui.py
+++ ui/gui.py
@@ -182,18 +182,12 @@ class Ui_MainWindow(object):
         self.horizontalLayout_6 = QtGui.QHBoxLayout()
         self.horizontalLayout_6.setSpacing(0)
         self.horizontalLayout_6.setObjectName(_fromUtf8("horizontalLayout_6"))
-        self.line_3 = QtGui.QFrame(self.onlineGroup)
-        self.line_3.setStyleSheet(_fromUtf8("border: 1px ridge #2c3e50;"))
-        self.line_3.setFrameShape(QtGui.QFrame.VLine)
-        self.line_3.setFrameShadow(QtGui.QFrame.Sunken)
-        self.line_3.setObjectName(_fromUtf8("line_3"))
-        self.horizontalLayout_6.addWidget(self.line_3)
         self.viewLogsButton = QtGui.QPushButton(self.onlineGroup)
         self.viewLogsButton.setFocusPolicy(QtCore.Qt.NoFocus)
         self.viewLogsButton.setStyleSheet(_fromUtf8("QPushButton#viewLogsButton {\n"
 "            border: none;\n"
 "            border-radius: none;\n"
-"            padding: 5px;\n"
+"            padding: 3px;\n"
 "            background-color: #34495e;\n"
 "            }\n"
 "\n"
@@ -204,7 +198,7 @@ class Ui_MainWindow(object):
         icon1 = QtGui.QIcon()
         icon1.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/log_viewer.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.viewLogsButton.setIcon(icon1)
-        self.viewLogsButton.setIconSize(QtCore.QSize(18, 18))
+        self.viewLogsButton.setIconSize(QtCore.QSize(16, 16))
         self.viewLogsButton.setObjectName(_fromUtf8("viewLogsButton"))
         self.horizontalLayout_6.addWidget(self.viewLogsButton)
         self.logSettingsButton = QtGui.QPushButton(self.onlineGroup)
@@ -212,7 +206,7 @@ class Ui_MainWindow(object):
         self.logSettingsButton.setStyleSheet(_fromUtf8("QPushButton#logSettingsButton {\n"
 "            border: none;\n"
 "            border-radius: none;\n"
-"            padding: 5px;\n"
+"            padding: 3px;\n"
 "            background-color: #34495e;\n"
 "            }\n"
 "\n"
@@ -223,7 +217,7 @@ class Ui_MainWindow(object):
         icon2 = QtGui.QIcon()
         icon2.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/other_settings.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.logSettingsButton.setIcon(icon2)
-        self.logSettingsButton.setIconSize(QtCore.QSize(18, 18))
+        self.logSettingsButton.setIconSize(QtCore.QSize(16, 16))
         self.logSettingsButton.setObjectName(_fromUtf8("logSettingsButton"))
         self.horizontalLayout_6.addWidget(self.logSettingsButton)
         self.line_2 = QtGui.QFrame(self.onlineGroup)
@@ -248,7 +242,7 @@ class Ui_MainWindow(object):
         icon3 = QtGui.QIcon()
         icon3.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/set_alias.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.setAliasButton.setIcon(icon3)
-        self.setAliasButton.setIconSize(QtCore.QSize(18, 18))
+        self.setAliasButton.setIconSize(QtCore.QSize(16, 16))
         self.setAliasButton.setObjectName(_fromUtf8("setAliasButton"))
         self.horizontalLayout_6.addWidget(self.setAliasButton)
         self.updateSourceButton = QtGui.QPushButton(self.onlineGroup)
@@ -256,7 +250,7 @@ class Ui_MainWindow(object):
         self.updateSourceButton.setStyleSheet(_fromUtf8("QPushButton#updateSourceButton {\n"
 "            border: none;\n"
 "            border-radius: none;\n"
-"            padding: 5px;\n"
+"            padding: 3px;\n"
 "            background-color: #34495e;\n"
 "            }\n"
 "\n"
@@ -267,7 +261,7 @@ class Ui_MainWindow(object):
         icon4 = QtGui.QIcon()
         icon4.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/update_source.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.updateSourceButton.setIcon(icon4)
-        self.updateSourceButton.setIconSize(QtCore.QSize(18, 18))
+        self.updateSourceButton.setIconSize(QtCore.QSize(16, 16))
         self.updateSourceButton.setObjectName(_fromUtf8("updateSourceButton"))
         self.horizontalLayout_6.addWidget(self.updateSourceButton)
         self.line = QtGui.QFrame(self.onlineGroup)
@@ -281,7 +275,7 @@ class Ui_MainWindow(object):
         self.shellButton.setStyleSheet(_fromUtf8("QPushButton#shellButton {\n"
 "            border: none;\n"
 "            border-radius: none;\n"
-"            padding: 5px;\n"
+"            padding: 3px;\n"
 "            background-color: #34495e;\n"
 "            }\n"
 "\n"
@@ -292,7 +286,7 @@ class Ui_MainWindow(object):
         icon5 = QtGui.QIcon()
         icon5.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/remote_shell.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.shellButton.setIcon(icon5)
-        self.shellButton.setIconSize(QtCore.QSize(18, 18))
+        self.shellButton.setIconSize(QtCore.QSize(16, 16))
         self.shellButton.setObjectName(_fromUtf8("shellButton"))
         self.horizontalLayout_6.addWidget(self.shellButton)
         self.explorerButton = QtGui.QPushButton(self.onlineGroup)
@@ -300,7 +294,7 @@ class Ui_MainWindow(object):
         self.explorerButton.setStyleSheet(_fromUtf8("QPushButton#explorerButton {\n"
 "            border: none;\n"
 "            border-radius: none;\n"
-"            padding: 5px;\n"
+"            padding: 3px;\n"
 "            background-color: #34495e;\n"
 "            }\n"
 "\n"
@@ -311,7 +305,7 @@ class Ui_MainWindow(object):
         icon6 = QtGui.QIcon()
         icon6.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/remote_explorer.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.explorerButton.setIcon(icon6)
-        self.explorerButton.setIconSize(QtCore.QSize(18, 18))
+        self.explorerButton.setIconSize(QtCore.QSize(16, 16))
         self.explorerButton.setObjectName(_fromUtf8("explorerButton"))
         self.horizontalLayout_6.addWidget(self.explorerButton)
         self.scriptingButton = QtGui.QPushButton(self.onlineGroup)
@@ -319,7 +313,7 @@ class Ui_MainWindow(object):
         self.scriptingButton.setStyleSheet(_fromUtf8("QPushButton#scriptingButton {\n"
 "            border: none;\n"
 "            border-radius: none;\n"
-"            padding: 5px;\n"
+"            padding: 3px;\n"
 "            background-color: #34495e;\n"
 "            }\n"
 "\n"
@@ -330,7 +324,7 @@ class Ui_MainWindow(object):
         icon7 = QtGui.QIcon()
         icon7.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/remote_scripting.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.scriptingButton.setIcon(icon7)
-        self.scriptingButton.setIconSize(QtCore.QSize(18, 18))
+        self.scriptingButton.setIconSize(QtCore.QSize(16, 16))
         self.scriptingButton.setObjectName(_fromUtf8("scriptingButton"))
         self.horizontalLayout_6.addWidget(self.scriptingButton)
         self.screenshotButton = QtGui.QPushButton(self.onlineGroup)
@@ -338,7 +332,7 @@ class Ui_MainWindow(object):
         self.screenshotButton.setStyleSheet(_fromUtf8("QPushButton#screenshotButton {\n"
 "            border: none;\n"
 "            border-radius: none;\n"
-"            padding: 5px;\n"
+"            padding: 3px;\n"
 "            background-color: #34495e;\n"
 "            }\n"
 "\n"
@@ -349,7 +343,7 @@ class Ui_MainWindow(object):
         icon8 = QtGui.QIcon()
         icon8.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/desktop.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.screenshotButton.setIcon(icon8)
-        self.screenshotButton.setIconSize(QtCore.QSize(18, 18))
+        self.screenshotButton.setIconSize(QtCore.QSize(16, 16))
         self.screenshotButton.setObjectName(_fromUtf8("screenshotButton"))
         self.horizontalLayout_6.addWidget(self.screenshotButton)
         self.webcamButton = QtGui.QPushButton(self.onlineGroup)
@@ -357,7 +351,7 @@ class Ui_MainWindow(object):
         self.webcamButton.setStyleSheet(_fromUtf8("QPushButton#webcamButton {\n"
 "            border: none;\n"
 "            border-radius: none;\n"
-"            padding: 5px;\n"
+"            padding: 3px;\n"
 "            background-color: #34495e;\n"
 "            }\n"
 "\n"
@@ -368,7 +362,7 @@ class Ui_MainWindow(object):
         icon9 = QtGui.QIcon()
         icon9.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/webcam.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.webcamButton.setIcon(icon9)
-        self.webcamButton.setIconSize(QtCore.QSize(18, 18))
+        self.webcamButton.setIconSize(QtCore.QSize(16, 16))
         self.webcamButton.setObjectName(_fromUtf8("webcamButton"))
         self.horizontalLayout_6.addWidget(self.webcamButton)
         spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
@@ -378,7 +372,7 @@ class Ui_MainWindow(object):
         self.setModeratorButton.setStyleSheet(_fromUtf8("QPushButton#setModeratorButton {\n"
 "            border: none;\n"
 "            border-radius: none;\n"
-"            padding: 5px;\n"
+"            padding: 3px;\n"
 "            background-color: #34495e;\n"
 "            }\n"
 "\n"
@@ -389,7 +383,7 @@ class Ui_MainWindow(object):
         icon10 = QtGui.QIcon()
         icon10.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/set_moderator.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.setModeratorButton.setIcon(icon10)
-        self.setModeratorButton.setIconSize(QtCore.QSize(18, 18))
+        self.setModeratorButton.setIconSize(QtCore.QSize(16, 16))
         self.setModeratorButton.setObjectName(_fromUtf8("setModeratorButton"))
         self.horizontalLayout_6.addWidget(self.setModeratorButton)
         self.gridLayout_6.addLayout(self.horizontalLayout_6, 0, 0, 1, 1)
@@ -494,7 +488,7 @@ class Ui_MainWindow(object):
         self.viewOfflineLogsButton.setStyleSheet(_fromUtf8("QPushButton#viewOfflineLogsButton {\n"
 "            border: none;\n"
 "            border-radius: none;\n"
-"            padding: 5px;\n"
+"            padding: 3px;\n"
 "            background-color: #34495e;\n"
 "            }\n"
 "\n"
@@ -503,7 +497,7 @@ class Ui_MainWindow(object):
 "            }"))
         self.viewOfflineLogsButton.setText(_fromUtf8(""))
         self.viewOfflineLogsButton.setIcon(icon1)
-        self.viewOfflineLogsButton.setIconSize(QtCore.QSize(18, 18))
+        self.viewOfflineLogsButton.setIconSize(QtCore.QSize(16, 16))
         self.viewOfflineLogsButton.setObjectName(_fromUtf8("viewOfflineLogsButton"))
         self.horizontalLayout_7.addWidget(self.viewOfflineLogsButton)
         self.setOfflineAliasButton = QtGui.QPushButton(self.offlineGroup)
@@ -511,7 +505,7 @@ class Ui_MainWindow(object):
         self.setOfflineAliasButton.setStyleSheet(_fromUtf8("QPushButton#setOfflineAliasButton {\n"
 "            border: none;\n"
 "            border-radius: none;\n"
-"            padding: 5px;\n"
+"            padding: 3px;\n"
 "            background-color: #34495e;\n"
 "            }\n"
 "\n"
@@ -520,7 +514,7 @@ class Ui_MainWindow(object):
 "            }"))
         self.setOfflineAliasButton.setText(_fromUtf8(""))
         self.setOfflineAliasButton.setIcon(icon3)
-        self.setOfflineAliasButton.setIconSize(QtCore.QSize(18, 18))
+        self.setOfflineAliasButton.setIconSize(QtCore.QSize(16, 16))
         self.setOfflineAliasButton.setObjectName(_fromUtf8("setOfflineAliasButton"))
         self.horizontalLayout_7.addWidget(self.setOfflineAliasButton)
         self.removeClientButton = QtGui.QPushButton(self.offlineGroup)
@@ -528,7 +522,7 @@ class Ui_MainWindow(object):
         self.removeClientButton.setStyleSheet(_fromUtf8("QPushButton#removeModeratorButton {\n"
 "            border: none;\n"
 "            border-radius: none;\n"
-"            padding: 5px;\n"
+"            padding: 3px;\n"
 "            background-color: #34495e;\n"
 "            }\n"
 "\n"
@@ -539,7 +533,7 @@ class Ui_MainWindow(object):
         icon12 = QtGui.QIcon()
         icon12.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/trash.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.removeClientButton.setIcon(icon12)
-        self.removeClientButton.setIconSize(QtCore.QSize(18, 18))
+        self.removeClientButton.setIconSize(QtCore.QSize(16, 16))
         self.removeClientButton.setObjectName(_fromUtf8("removeClientButton"))
         self.horizontalLayout_7.addWidget(self.removeClientButton)
         spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
@@ -635,7 +629,7 @@ class Ui_MainWindow(object):
         self.addModeratorButton.setStyleSheet(_fromUtf8("QPushButton#addModeratorButton {\n"
 "            border: none;\n"
 "            border-radius: none;\n"
-"            padding: 5px;\n"
+"            padding: 3px;\n"
 "            background-color: #34495e;\n"
 "            }\n"
 "\n"
@@ -646,14 +640,14 @@ class Ui_MainWindow(object):
         icon14 = QtGui.QIcon()
         icon14.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/add_moderator.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.addModeratorButton.setIcon(icon14)
-        self.addModeratorButton.setIconSize(QtCore.QSize(18, 18))
+        self.addModeratorButton.setIconSize(QtCore.QSize(16, 16))
         self.addModeratorButton.setObjectName(_fromUtf8("addModeratorButton"))
         self.horizontalLayout_8.addWidget(self.addModeratorButton)
         self.changePasswordButton = QtGui.QPushButton(self.moderatorsGroup)
         self.changePasswordButton.setStyleSheet(_fromUtf8("QPushButton#changePasswordButton {\n"
 "            border: none;\n"
 "            border-radius: none;\n"
-"            padding: 5px;\n"
+"            padding: 3px;\n"
 "            background-color: #34495e;\n"
 "            }\n"
 "\n"
@@ -664,14 +658,14 @@ class Ui_MainWindow(object):
         icon15 = QtGui.QIcon()
         icon15.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/password.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.changePasswordButton.setIcon(icon15)
-        self.changePasswordButton.setIconSize(QtCore.QSize(18, 18))
+        self.changePasswordButton.setIconSize(QtCore.QSize(16, 16))
         self.changePasswordButton.setObjectName(_fromUtf8("changePasswordButton"))
         self.horizontalLayout_8.addWidget(self.changePasswordButton)
         self.changePrivilegesButton = QtGui.QPushButton(self.moderatorsGroup)
         self.changePrivilegesButton.setStyleSheet(_fromUtf8("QPushButton#changePrivilegesButton {\n"
 "            border: none;\n"
 "            border-radius: none;\n"
-"            padding: 5px;\n"
+"            padding: 3px;\n"
 "            background-color: #34495e;\n"
 "            }\n"
 "\n"
@@ -682,23 +676,23 @@ class Ui_MainWindow(object):
         icon16 = QtGui.QIcon()
         icon16.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/privileges.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.changePrivilegesButton.setIcon(icon16)
-        self.changePrivilegesButton.setIconSize(QtCore.QSize(18, 18))
+        self.changePrivilegesButton.setIconSize(QtCore.QSize(16, 16))
         self.changePrivilegesButton.setObjectName(_fromUtf8("changePrivilegesButton"))
         self.horizontalLayout_8.addWidget(self.changePrivilegesButton)
         self.removeModeratorButton = QtGui.QPushButton(self.moderatorsGroup)
-        self.removeModeratorButton.setStyleSheet(_fromUtf8("QPushButton#remoteModerator {\n"
+        self.removeModeratorButton.setStyleSheet(_fromUtf8("QPushButton#removeModeratorButton {\n"
 "            border: none;\n"
 "            border-radius: none;\n"
-"            padding: 5px;\n"
+"            padding: 3px;\n"
 "            background-color: #34495e;\n"
 "            }\n"
 "\n"
-"QPushButton#remoteModerator:pressed {\n"
+"QPushButton#removeModeratorButton:pressed {\n"
 "            background-color: #2c3e50;\n"
 "            }"))
         self.removeModeratorButton.setText(_fromUtf8(""))
         self.removeModeratorButton.setIcon(icon12)
-        self.removeModeratorButton.setIconSize(QtCore.QSize(18, 18))
+        self.removeModeratorButton.setIconSize(QtCore.QSize(16, 16))
         self.removeModeratorButton.setObjectName(_fromUtf8("removeModeratorButton"))
         self.horizontalLayout_8.addWidget(self.removeModeratorButton)
         spacerItem2 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
@@ -774,10 +768,13 @@ class Ui_MainWindow(object):
         self.clientsTabs.addTab(self.moderatorsTab, icon17, _fromUtf8(""))
         self.gridLayout.addWidget(self.clientsTabs, 1, 0, 1, 1)
         self.groupBox = QtGui.QGroupBox(self.centralwidget)
-        self.groupBox.setStyleSheet(_fromUtf8("background-color: #34495e; \n"
+        self.groupBox.setStyleSheet(_fromUtf8("background-color: #2c3e50; \n"
 "border: none;\n"
-"margin-left: 10px;\n"
-"margin-right: 10px;"))
+"margin-left: 6px;\n"
+"margin-right: 6px;\n"
+"padding: -5px;\n"
+"padding-top: -7px;\n"
+"padding-bottom: -7px;"))
         self.groupBox.setTitle(_fromUtf8(""))
         self.groupBox.setObjectName(_fromUtf8("groupBox"))
         self.gridLayout_5 = QtGui.QGridLayout(self.groupBox)
@@ -789,7 +786,9 @@ class Ui_MainWindow(object):
         self.connectButton.setMinimumSize(QtCore.QSize(0, 0))
         self.connectButton.setMaximumSize(QtCore.QSize(16777215, 16777215))
         self.connectButton.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.connectButton.setToolTip(_fromUtf8(""))
         self.connectButton.setStyleSheet(_fromUtf8("QPushButton#connectButton {\n"
+"            background-color: #34495e;\n"
 "            border: none;\n"
 "            border-radius: none;\n"
 "            padding: 5px;\n"
@@ -807,7 +806,7 @@ class Ui_MainWindow(object):
         icon18 = QtGui.QIcon()
         icon18.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/connect.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.connectButton.setIcon(icon18)
-        self.connectButton.setIconSize(QtCore.QSize(18, 18))
+        self.connectButton.setIconSize(QtCore.QSize(16, 16))
         self.connectButton.setCheckable(True)
         self.connectButton.setObjectName(_fromUtf8("connectButton"))
         self.horizontalLayout.addWidget(self.connectButton)
@@ -816,6 +815,7 @@ class Ui_MainWindow(object):
         self.disconnectButton.setMaximumSize(QtCore.QSize(16777215, 16777215))
         self.disconnectButton.setFocusPolicy(QtCore.Qt.NoFocus)
         self.disconnectButton.setStyleSheet(_fromUtf8("QPushButton#disconnectButton {\n"
+"            background-color: #34495e;\n"
 "            border: none;\n"
 "            border-radius: none;\n"
 "            padding: 5px;\n"
@@ -829,7 +829,7 @@ class Ui_MainWindow(object):
         icon19 = QtGui.QIcon()
         icon19.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/disconnect.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.disconnectButton.setIcon(icon19)
-        self.disconnectButton.setIconSize(QtCore.QSize(18, 18))
+        self.disconnectButton.setIconSize(QtCore.QSize(16, 16))
         self.disconnectButton.setCheckable(False)
         self.disconnectButton.setChecked(False)
         self.disconnectButton.setObjectName(_fromUtf8("disconnectButton"))
@@ -838,6 +838,7 @@ class Ui_MainWindow(object):
         self.horizontalLayout.addItem(spacerItem3)
         self.settingsButton = QtGui.QPushButton(self.groupBox)
         self.settingsButton.setStyleSheet(_fromUtf8("QPushButton#settingsButton {\n"
+"            background-color: #34495e;\n"
 "            border: none;\n"
 "            border-radius: none;\n"
 "            padding: 5px;\n"
@@ -855,6 +856,7 @@ class Ui_MainWindow(object):
         icon20 = QtGui.QIcon()
         icon20.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/settings.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.settingsButton.setIcon(icon20)
+        self.settingsButton.setIconSize(QtCore.QSize(16, 16))
         self.settingsButton.setCheckable(True)
         self.settingsButton.setChecked(False)
         self.settingsButton.setObjectName(_fromUtf8("settingsButton"))

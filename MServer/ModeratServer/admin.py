91c8ff385c613b685dca80993a47be4a1ee91736
uripa <uripatton@gmail.com>
2017-02-22 00:13:19 +0400

    Disable Add Permission For Screenshots

--- MServer/ModeratServer/admin.py
+++ MServer/ModeratServer/admin.py
@@ -59,6 +59,10 @@ class ClientsAdmin(admin.ModelAdmin):
 
 
 class ScreenshotsAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
+        return False
+
     search_fields = ['client_id__alias', 'title_name', 'client_id__moderator_id__username']
     list_filter = ['client_id__moderator_id__username', 'viewed', 'date']
     list_display = ['path', 'title_name', 'date', 'viewed']
